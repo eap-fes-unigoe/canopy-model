c5660c8088b4fb3b9e71af67b35939abd9440d81
CheyenneRL <c.ruedalagasse@stud.uni-goettingen.de>
2021-02-13 18:28:32 +0100

    Update run_model.R

--- SoilTemperature_SoilMoisture/Optimization_SoilTemp/run_model.R
+++ SoilTemperature_SoilMoisture/Optimization_SoilTemp/run_model.R
@@ -3,7 +3,7 @@
 parsfile <- "pars_soils_calib1.csv"
 source("setup_soiltemperature_calib.R")
 source("fun_soiltemperature1.R")
-
+Tsubsoil <- 283
 output <- get_soiltemp(input = input, initial_state = initial_state, pars = pars)
 
 plot(fluxes$tsoil, ylim = c(284, 292))
